16cf01f31479d7a5982dd22760a726cdfd8ee912
Abdul Wahid <a.wahidchohan@gmail.com>
2025-11-25 22:50:33 +0500

    Create Transaction model with relationships

---
+++ app/Models/Transaction.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Transaction extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'sender_id',
+        'receiver_id',
+        'amount',
+        'commission_fee',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'amount' => 'decimal:2',
+            'commission_fee' => 'decimal:2',
+        ];
+    }
+
+    /**
+     * Get the user who sent the transaction.
+     */
+    public function sender(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
+
+    /**
+     * Get the user who received the transaction.
+     */
+    public function receiver(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'receiver_id');
+    }
+}
+
