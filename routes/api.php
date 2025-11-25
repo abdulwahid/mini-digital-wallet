a76a3d8636bf23e1bc3006cc91a6028b3cb5c6d2
Abdul Wahid <a.wahidchohan@gmail.com>
2025-11-25 22:56:18 +0500

    Set up API routes structure

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,19 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Public routes (if any)
+// Route::post('/register', ...);
+// Route::post('/login', ...);
+
+// Authenticated routes
+Route::middleware('auth:sanctum')->group(function () {
+    // User info endpoint
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    // Transaction routes (to be implemented)
+    // Route::get('/transactions', [TransactionController::class, 'index']);
+    // Route::post('/transactions', [TransactionController::class, 'store']);
+});
 
